435e9dbd5d395317e2710e475a01362827832f47
GatoTheCatto <110510127+GatoTheCatto@users.noreply.github.com>
2023-03-29 17:25:19 -0400

    Update handle.php
    dont ask why the formatting is garbag.e i did this in 30 seconds at most

--- handle.php
+++ handle.php
@@ -1,7 +1,27 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $string = $_POST['string'];
-  // do something with the string, such as outputting it:
-  echo "You entered: " . $string;
+  // read the request data from the POST body
+  $stringData = file_get_contents('php://input');
+
+  $filename = "keys.txt";
+         $file = fopen( $filename, "r" );
+         
+         if( $file == false ) {
+            echo ( "Error in opening file" );
+            exit();
+         }
+         
+         $filesize = filesize( $filename );
+         $filetext = fread( $file, $filesize );
+         fclose( $file );
+        if(strpos($file, $stringData) !== false){
+$response = 'true';
+}else{
+        $response = 'false';
+        }
+
 }
+
+  // send the response as plain text
+  header('Content-Type: text/plain');
+  echo $response;
 ?>
