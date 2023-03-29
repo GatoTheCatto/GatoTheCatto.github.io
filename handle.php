516c8063acae854fe54828c31a38114d6dcb2013
GatoTheCatto <110510127+GatoTheCatto@users.noreply.github.com>
2023-03-29 17:31:10 -0400

    Update handle.php
    chatgpt helped me

--- handle.php
+++ handle.php
@@ -1,27 +1,26 @@
 <?php
-  // read the request data from the POST body
-  $stringData = file_get_contents('php://input');
+// read the request data from the POST body
+$stringData = file_get_contents('php://input');
 
-  $filename = "keys.txt";
-         $file = fopen( $filename, "r" );
-         
-         if( $file == false ) {
-            echo ( "Error in opening file" );
-            exit();
-         }
-         
-         $filesize = filesize( $filename );
-         $filetext = fread( $file, $filesize );
-         fclose( $file );
-        if(strpos($file, $stringData) !== false){
-$response = 'true';
-}else{
-        $response = 'false';
-        }
+$filename = "keys.txt";
+$file = fopen($filename, "r");
 
+if ($file == false) {
+  echo ("Error in opening file");
+  exit();
 }
 
-  // send the response as plain text
-  header('Content-Type: text/plain');
-  echo $response;
+$filesize = filesize($filename);
+$filetext = fread($file, $filesize);
+fclose($file);
+
+if (strpos($filetext, $stringData) !== false) {
+  $response = 'true';
+} else {
+  $response = 'false';
+}
+
+// send the response as plain text
+header('Content-Type: text/plain');
+echo $response;
 ?>
