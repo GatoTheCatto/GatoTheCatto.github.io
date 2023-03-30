00cae43352b92b0fbe8ecf57669ae8c18282c50c
GatoTheCatto <110510127+GatoTheCatto@users.noreply.github.com>
2023-03-30 15:40:08 -0400

    cahtgpt rexocede
    recode

--- handle.php
+++ handle.php
@@ -1,9 +1,11 @@
+<?php
+// Set the allowed origins
 header("Access-Control-Allow-Origin: *");
 
-<?php
-// read the request data from the query parameters
+// Read the request data from the query parameters
 $stringData = $_GET['key'];
 
+// Open the file containing the keys
 $filename = "keys.txt";
 $file = fopen($filename, "r");
 
@@ -16,13 +18,14 @@ $filesize = filesize($filename);
 $filetext = fread($file, $filesize);
 fclose($file);
 
+// Check if the requested key is present in the file
 if (strpos($filetext, $stringData) !== false) {
   $response = 'true';
 } else {
   $response = 'false';
 }
 
-// send the response as plain text
+// Send the response as plain text
 header('Content-Type: text/plain');
 echo $response;
 ?>
